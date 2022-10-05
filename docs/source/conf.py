6c107a197490d1c1e5726c68193f30aceeda2b08
Collin Myhre <collin.myhre@gmail.com>
2022-10-04 21:30:07 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'TIMELAPSE Tool'
+copyright = '2022, Fwiller'
+author = 'Fwiller'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0.0'
+version = '1.0.0'
 
 # -- General configuration
 
